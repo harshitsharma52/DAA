f8d87f4c288af63bb9c66704ec919f098701e248
harshitsharma52 <harshit.sharma6091@gmail.com>
2021-08-27 00:03:50 +0530

    sorting

---
+++ LAB_WORK/week02/sortedcountduplicate.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+int  Binary_search(int arr[], int left,int right,int key)
+{
+
+    
+    if(right >= left)
+    {
+        int m = (left+right)/2;
+        if(arr[m] == key)
+        {
+             while(arr[m] == key)
+            {
+                if(m == -1)
+                    break;
+                m--;
+            }
+            return m+=1;
+        }
+        
+        if(arr[m] > key)
+            return Binary_search(arr,left,m-1,key);
+        else  
+            return Binary_search(arr,m+1,right,key);
+    }
+    else
+        return -1;
+    
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,j=0;
+        cin>>n;
+        int arr[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin>>arr[i];
+        }
+        int key;
+        cin>>key;
+        int result = Binary_search(arr,0,n-1,key);
+     
+        if(result >= 0)
+        {
+            j=result+1;
+            while(arr[j]==key)
+             j++;
+            
+            cout<<key<<"-"<<j-result<<"\n";
+        }
+        else
+            cout<<"Not Present "<<"\n";
+            j=0;
+    }
+    return 0;
+}
+
